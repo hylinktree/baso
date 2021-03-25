9b620daa390e507cea245b5535325675b4762025
朱玄義 <hsiuenyichu@insnergy.com>
2021-03-25 10:17:45 +0800

    nocomment

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo Baso Installer 2021-03-25
 docker pull hylinktree/basoback_hy
+docker stop basoback_hy
+docker rm basoback_hy
+docker run --name basoback_hy --restart always -p 8603:8603 hylinktree/basoback_hy
 
