460257d70829c65089a335650671436b90c17ea6
朱玄義 <hsiuenyichu@insnergy.com>
2021-03-25 15:20:10 +0800

    nocomment

--- install.bat
+++ install.bat
@@ -1,6 +1,15 @@
 echo Baso Installer 2021/03/25a
 set docker_dns=--add-host db.pri.io:192.168.1.12 --add-host db.sec.io:192.168.5.27
 set gitbase=hylinktree
+
+docker network create baso
+
+set tgt=basoweb
+docker pull %gitbase%/%tgt%
+docker stop %tgt%
+docker rm %tgt%
+docker run --name %tgt% -d --restart always %docker_dns% -p 8668:8668 %gitbase%/%tgt%
+
 set tgt=basoback_hy
 set reposrc=d:\apps\repo
 set repodir=/mnt/baso/.repo
