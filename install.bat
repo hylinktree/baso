460f74c5bf6ade3681bb39c94fb300d82fb93bd9
hylinktree <hylinktree@gmail.com>
2021-03-25 10:34:15 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,1 +1,5 @@
+echo Baso Installer 2021/03/25
 docker pull hylinktree/basoback_hy
+docker stop basoback_hy
+docker rm basoback_hy
+docker run --name basoback_hy --restart always -v d:\apps\repo:/mnt/basorepo -p 8603:8603 hylinktree/basoback_hy
